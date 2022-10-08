8578b7c0fbdccea4f06cfb90f1e60f8ffce277e7
Bryan Jennings <bryanjenningz@gmail.com>
2022-10-08 11:08:37 -0700

    Add add_3

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub mod rs0 {
     pub fn add(a: i32, b: i32) -> i32 {
         a + b
     }
+
+    pub fn add_3(a: i32, b: i32, c: i32) -> i32 {
+        a + b + c
+    }
 }
 
 #[cfg(test)]
@@ -15,5 +19,14 @@ mod test_rs0 {
         assert_eq!(add(20, 0), 20);
         assert_eq!(add(-20, 20), 0);
         assert_eq!(add(-2, 20), 18);
+    }
+
+    #[test]
+    fn test_add_3() {
+        assert_eq!(add_3(0, 0, 0), 0);
+        assert_eq!(add_3(0, 1, 20), 21);
+        assert_eq!(add_3(20, 0, -100), -80);
+        assert_eq!(add_3(-20, 0, 20), 0);
+        assert_eq!(add_3(-2, 20, 200), 218);
     }
 }
